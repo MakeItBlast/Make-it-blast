c25b7d69f8817d6a93d2e43f1d28f6f845176fc7
MakeItBlast <blastitnowllc@gmail.com>
2025-05-10 01:29:31 +0530

    Add files via upload

---
+++ app/Http/Controllers/Admin/AdminBlastController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller; // Import Base Controller
+
+use App\Models\Service;
+use App\Models\SystemValue;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AdminBlastController extends Controller
+{
+    public function createUpdateBlast(Request $request){
+
+    }
+}
